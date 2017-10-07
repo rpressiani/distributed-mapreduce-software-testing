dcfcdfb14a889b8e1f2d0dda9a2131c3e533a446
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2017-10-07 15:16:46 -0500

    Fix Reduce to show tests list

--- mapreduce-task/src/main/java/Reduce.java
+++ mapreduce-task/src/main/java/Reduce.java
@@ -1,23 +1,31 @@
-import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Reduce
-        extends Reducer<LineWritable, Text, LineWritable, MapWritable> {
+        extends Reducer<LineWritable, Text, LineWritable, Text> {
 
-    public void reduce(LineWritable key, Iterable<Text> values,
-                       Context context
-    ) throws IOException, InterruptedException {
+    public void reduce(LineWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
-        MapWritable result = new MapWritable();
+        ArrayList<String> tests = new ArrayList();
         int count = 0;
         for (Text value : values) {
-            count = count + 1;
-            result.put(new Text(value), new IntWritable(count));
+            count++;
+            tests.add(value.toString());
         }
 
-        context.write(key, result);
+        StringBuilder result = new StringBuilder();
+
+        for (String test : tests) {
+            result.append(test);
+            result.append(",");
+        }
+
+        result.append(count);
+
+        context.write(key, new Text(result.toString()));
     }
 
 }
