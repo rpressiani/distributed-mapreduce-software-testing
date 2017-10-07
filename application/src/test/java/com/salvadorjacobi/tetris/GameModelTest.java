5dd5404ba4a98bda3a0d839cfc6739c3d6bca89a
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2017-10-07 16:18:18 -0500

    Add tests

--- application/src/test/java/com/salvadorjacobi/tetris/GameModelTest.java
+++ application/src/test/java/com/salvadorjacobi/tetris/GameModelTest.java
@@ -3,6 +3,8 @@ package com.salvadorjacobi.tetris;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.*;
+
 import static org.junit.Assert.assertEquals;
 
 public class GameModelTest {
@@ -14,24 +16,49 @@ public class GameModelTest {
         model = new GameModel(10, 20, 32);
     }
 
-//    @Test
-//    public void swappedTetriminoWillComeBack() throws Exception {
-//        Tetrimino fallingTetrimino = model.getFallingTetrimino();
-//
-//        model.swap();
-//        model.hardDrop();
-//        model.swap();
-//
-//        assertEquals("Previously swapped Tetrimino has to come back in the next swap",
-//                fallingTetrimino, model.getFallingTetrimino());
-//    }
+    @Test
+    public void swappedTetriminoWillComeBack() throws Exception {
+        Tetrimino fallingTetrimino = model.getFallingTetrimino();
 
-//    @Test
-//    public void fallingTetriminoCanBeSwappedOnlyOnce() throws Exception {
-//        model.swap();
-//        Tetrimino newFallingTetrimino = model.getFallingTetrimino();
-//        model.swap();
-//
-//        assertEquals(newFallingTetrimino, model.getFallingTetrimino());
-//    }
+        model.swap();
+        model.hardDrop();
+        model.swap();
+
+        assertEquals("Previously swapped Tetrimino has to come back in the next swap",
+                fallingTetrimino, model.getFallingTetrimino());
+    }
+
+    @Test
+    public void fallingTetriminoCanBeSwappedOnlyOnce() throws Exception {
+        model.swap();
+        Tetrimino newFallingTetrimino = model.getFallingTetrimino();
+        model.swap();
+
+        assertEquals(newFallingTetrimino, model.getFallingTetrimino());
+    }
+
+    @Test
+    public void shiftTrueMoveTetriminoLeft() throws Exception {
+        int startingPos = model.getFallingTetrimino().getPosition().x;
+        model.shift(true);
+
+        assertEquals(model.getFallingTetrimino().getPosition().x + 1, startingPos);
+    }
+
+    @Test
+    public void shiftFalseMoveTetriminoRight() throws Exception {
+        int startingPos = model.getFallingTetrimino().getPosition().x;
+        model.shift(false);
+
+        assertEquals(model.getFallingTetrimino().getPosition().x - 1, startingPos);
+    }
+
+    @Test
+    public void tetriminoCannotMoveOutsideBounds() throws Exception {
+        model.getFallingTetrimino().setPosition(new Point(0, 5));
+        int startingPos = model.getFallingTetrimino().getPosition().x;
+        model.shift(true);
+
+        assertEquals(model.getFallingTetrimino().getPosition().x, startingPos);
+    }
 }
--- application/src/test/java/com/salvadorjacobi/tetris/TetriminoTest.java
+++ application/src/test/java/com/salvadorjacobi/tetris/TetriminoTest.java
@@ -1,12 +1,12 @@
-import com.salvadorjacobi.tetris.Tetrimino;
-import org.junit.Before;
+package com.salvadorjacobi.tetris;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.awt.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class TetriminoTest {
 
@@ -16,19 +16,26 @@ public class TetriminoTest {
     public ExpectedException thrown = ExpectedException.none();
 
 
-//    @Test
-//    public void rotationCannotBeNegative() throws Exception {
-//        Tetrimino tetrimino = new Tetrimino(Tetrimino.Shape.S, new Point(0,0), 0);
-//
-//        tetrimino.rotate(false);
-//
-//        assertEquals("Rotation cannot be negative", 3, tetrimino.getRotation());
-//    }
+    @Test
+    public void rotationCannotBeNegative() throws Exception {
+        tetrimino = new Tetrimino(Tetrimino.Shape.S, new Point(0,0), 0);
+
+        tetrimino.rotate(false);
+
+        assertEquals("Rotation cannot be negative", 3, tetrimino.getRotation());
+    }
 
     @Test
     public void outOfBoundRotationNotAllowed() throws Exception {
         thrown.expect(IllegalArgumentException.class);
 
         new Tetrimino(Tetrimino.Shape.T, new Point(0,0), 6);
+    }
+
+    @Test
+    public void tetriminoPositionCannotBeNull() throws Exception {
+        thrown.expect(IllegalArgumentException.class);
+
+        new Tetrimino(Tetrimino.Shape.T, null, 1);
     }
 }
